1a7f76cab2be590155e392d46f9be6d5118286ce
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-01-10 00:07:38 +0530

    Update StaffRepository.java
    removed @Repository annotation

--- HospitalStaffManagement/src/main/java/com/demo/HospitalStaffManagement/repository/StaffRepository.java
+++ HospitalStaffManagement/src/main/java/com/demo/HospitalStaffManagement/repository/StaffRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface StaffRepository extends CrudRepository<Staff, Integer> {
     Optional<Staff> findByStaffEmail(String email);
 
