26f84d1b6a42971a654e16ffed4700b1af854595
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-01-10 00:09:48 +0530

    Update HospitalService.java
    removed @Service annotation

--- HospitalStaffManagement/src/main/java/com/demo/HospitalStaffManagement/service/HospitalService.java
+++ HospitalStaffManagement/src/main/java/com/demo/HospitalStaffManagement/service/HospitalService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.List;
 
-@Service
 public interface HospitalService {
 
     public List<HashMap<String, Object>> getAllAdmittedPatients();
