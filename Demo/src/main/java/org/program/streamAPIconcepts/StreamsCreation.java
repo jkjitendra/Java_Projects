c038d74b35681ffdab7971f7cbe5d9dbc18373e2
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-04-20 01:04:22 +0530

    Stream APIs -> stream creation
    Multiple ways to create stream using Collections, Arrays, Static Factory Methods, range method, I/O Channels (Files.lines(path))

---
+++ Demo/src/main/java/org/program/streamAPIconcepts/StreamsCreation.java
@@ -0,0 +1,71 @@
+package org.program.streamAPIconcepts;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+
+public class StreamsCreation {
+
+    public static void main(String[] args) {
+        // Creating Streams from Collections
+        List<String> list = Arrays.asList("Apple", "Banana", "Cherry", "Date");
+        Stream<String> streamFromList = list.stream();
+        streamFromList.forEach(System.out::print);
+        System.out.println();
+
+        // Creating Streams from Arrays
+        String[] array = new String[]{"Apple", "Banana", "Cherry", "Date"};
+        Stream<String> streamFromArray = Arrays.stream(array);
+        streamFromArray.forEach(System.out::print);
+        System.out.println();
+
+        // Creating Streams from Static Factory Methods
+        Stream<String> streamOfStrings = Stream.of("Apple", "Banana", "Cherry", "Date");
+        streamOfStrings.forEach(System.out::print);
+        System.out.println();
+
+        // For primitive types
+        IntStream streamOfInts = IntStream.of(1, 2, 3, 4);
+        streamOfInts.forEach(System.out::print);
+        System.out.println();
+
+        DoubleStream streamOfDoubles = DoubleStream.of(1, 2, 3, 4);
+        streamOfDoubles.forEach(System.out::print);
+        System.out.println();
+
+        // Using range
+        IntStream intRangeStream = IntStream.range(1, 5); // Generates numbers from 1 to 4
+        intRangeStream.forEach(System.out::print);
+        System.out.println();
+
+        //  Creating Streams from I/O Channels
+        // One common use case is to create a stream from the lines of a file, which can be done using Files.lines(Path path)
+        Path path = Paths.get("src/main/java/org/program/TrafficSignalSimple.java"); // Make sure to have this file in your working directory with some text.
+        // This method opens a file, reads all lines lazily, and creates a stream allowing you to process the file line by line.
+        // It's important to note that the Files.lines() method needs to be closed, which is why it is used within a try-with-resources statement here.
+        try (Stream<String> lines = Files.lines(path)) { //
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // To print comma separated values use
+
+        // Collectors.joining
+        IntStream rangeStream1 = IntStream.range(1, 5);
+        String result = rangeStream1.mapToObj(String::valueOf)
+                .collect(Collectors.joining(", "));
+        System.out.println(result); // Outputs: 1, 2, 3, 4
+
+        // or StringBuilder
+        StringBuilder sb = new StringBuilder();
+        IntStream rangeStream2 = IntStream.range(1, 5);
+        rangeStream2.forEach(value -> sb.append(value).append(", "));
+        if (sb.length() > 0) sb.setLength(sb.length() - 2); // Remove the last comma and space
+        System.out.println(sb.toString()); // Outputs: 1, 2, 3, 4
+    }
+}
